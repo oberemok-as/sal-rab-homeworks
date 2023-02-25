0e270d51c35e405a6c7514b446b83f37a859e0d5
Neto test <8495@bk.ru>
2023-02-26 01:02:16 +0300

    teatcher's comments

--- task_2/solution.js
+++ task_2/solution.js
@@ -6,11 +6,9 @@ function calcShipping(sum, min, shipping) {
     // Задание №2.1. Рассчитать доставку
     let shippingSum;
     
-    if(productsSum == 0) {
-    shippingSum = 0
-    }else if(productsSum >= freeShippingMinSum) {
+    if(productsSum == 0 || productsSum >= freeShippingMinSum) {
         shippingSum = 0
-    }else if(productsSum > 0 &&  productsSum < freeShippingMinSum){
+    }else {
         shippingSum = shippingPrice
     }
     // Конец решения задания №2.1.
@@ -24,12 +22,7 @@ function calcDiscount(sum, min, discount) {
     let discountPart = discount; // величина скидки в процентах
 
     // Задание №2.2. Рассчитать скидку
-    let discountSum;
-    if (productsSum >= discountMinSum) {
-        discountSum = productsSum*discountPart/100;
-    }else {
-        discountSum=0;
-    }
+    let discountSum = productsSum >= discountMinSum ? productsSum * discountPart / 100 : 0;
      // Конец решения задания №2.2.
 
     return discountSum;
--- task_3/solution.js
+++ task_3/solution.js
@@ -2,10 +2,7 @@ function renderCartItem(item) {
     const product = item;
 
     // Задание №3.1. Формирование строки корзины
-    let itemCountText = '';
-    itemCountText = itemCountText + product.count + ` × ` +  product.price + ` ₽ = `;
-    let sum = product.count * product.price
-    itemCountText = itemCountText + sum + ` ₽`;
+    let itemCountText = `${product.count} × ${product.price} ₽ = ${product.count * product.price} ₽`;
     // Конец решения задания №3.1.
 
     return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
